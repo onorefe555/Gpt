a20e88bad44958541b452cffe4fc8b37856139e3
onorefe555 <90953726+onorefe555@users.noreply.github.com>
2024-03-02 08:21:07 -0800

    Delete myresturant directory

--- myresturant/src/component/index.js
+++
@@ -1,4 +0,0 @@
-export {default as Footers} from "./Footers/Footers"
-export {default as Menuitem} from "./Menuitem/Menuitem"
-export {default as Navbar} from "./Navbar/Navbar"
-export {default as Subheading} from "./Subheading/Subheading"
